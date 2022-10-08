6d29bc6e8c434e7f387e3f59ccf71cf7a7c55f28
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-08 13:29:00 +0900

    feat: annotation

--- src/main/java/com/example/shoppingmall_restapi/ShoppingMallRestapiApplication.java
+++ src/main/java/com/example/shoppingmall_restapi/ShoppingMallRestapiApplication.java
@@ -2,7 +2,9 @@ package com.example.shoppingmall_restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ShoppingMallRestapiApplication {
 
