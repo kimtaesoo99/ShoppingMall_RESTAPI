1cfed7628d577ea51a62d7a2d5d3527796c8638c
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-10-31 00:20:36 +0900

    feat: ReportController

---
+++ src/main/java/com/example/shoppingmall_restapi/controller/report/ReportController.java
@@ -0,0 +1,36 @@
+package com.example.shoppingmall_restapi.controller.report;
+
+
+import com.example.shoppingmall_restapi.dto.report.MemberReportRequestDto;
+import com.example.shoppingmall_restapi.dto.report.ProductReportRequest;
+import com.example.shoppingmall_restapi.entity.member.Member;
+import com.example.shoppingmall_restapi.exception.MemberNotFoundException;
+import com.example.shoppingmall_restapi.repository.member.MemberRepository;
+import com.example.shoppingmall_restapi.response.Response;
+import com.example.shoppingmall_restapi.service.report.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ReportController {
+    private final ReportService reportService;
+    private final MemberRepository memberRepository;
+
+    //유저를 신고
+    @PostMapping("/report/users")
+    @ResponseStatus(HttpStatus.OK)
+    public Response reportMember(@Valid @RequestBody MemberReportRequestDto req){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Member member = memberRepository.findByUsername(authentication.getName()).orElseThrow(MemberNotFoundException::new);
+        reportService.reportMember(req,member);
+        return Response.success();
+    }
+
+}
